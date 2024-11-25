feed36d409f741ce40e8657894da6a27f701323e
collegevlogs <collegevlogs999@gmail.com>
2024-11-25 09:33:55 +0530

    Add files via upload

---
+++ pgrm5b.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+public class pgrm5b {
+    public static void main(String args[]) throws IOException,FileNotFoundException
+    {
+        char c;
+        int i;
+        FileInputStream fin=new FileInputStream("test1.txt");
+        FileOutputStream fout=new FileOutputStream("test1.txt");
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter characters, 'q' to quit.");
+        do{
+            c=(char)br.read();
+            if(c!='q')
+            fout.write(c);
+        }while(c!='q');
+        System.out.println("Contents of the file are");
+        do{
+            i=fin.read();
+            if(i!=-1)
+            System.out.print((char) i);
+
+
+        }while(i != -1);
+        fin.close();
+        fout.close();
+    }
+}
